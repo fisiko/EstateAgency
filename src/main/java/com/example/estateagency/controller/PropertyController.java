c69bb348975b15e09edfa2d41d980a6fb294ce6f
fisik <fisiko2000@gmail.com>
2023-05-15 08:59:03 +0100

    Commit 2

--- src/main/java/com/example/estateagency/controller/PropertyController.java
+++ src/main/java/com/example/estateagency/controller/PropertyController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/property")
 public class PropertyController {
